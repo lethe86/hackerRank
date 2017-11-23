a82a13a940c823eb1b64f8876e0e4b1806c73149
lethe86 <lethe_86@naver.com>
2017-11-23 13:16:06 +0900

    Beautiful Days at the Movies

---
+++ Algorithms/Implementation/Beautiful Days at the Movies.js
@@ -0,0 +1,29 @@
+function processData(input) {
+    //Enter your code here
+    let inputData = [];   
+    let eachData = [];
+    let charData = [];
+    let reverseData = [];
+    let n = 0;
+    let cnt = 0;
+    
+    inputData = input.split(' ');
+    n = parseInt(inputData[1]-inputData[0]+1);
+    
+    for (let i=0; i<n; i++) {        
+        eachData = inputData[0]++ + "";  
+        for (let j=0; j<eachData.length; j++) {
+            charData[j] = eachData[j];
+        }    
+        
+        charData.reverse();        
+        for (let j=0; j<eachData.length; j++) {
+            reverseData += charData[j];
+        }        
+        
+        if (Number.isInteger(Math.abs(eachData - reverseData) / inputData[2]))
+            cnt++;
+        reverseData = [];            
+    }    
+    console.log(cnt);    
+} 
