5673be62b5be21252da82ec1fe78a35376b1767f
lethe86 <lethe_86@naver.com>
2017-11-23 13:56:05 +0900

    Beautiful Days at the Movies - Other solution

--- Algorithms/Implementation/Beautiful Days at the Movies.js
+++ Algorithms/Implementation/Beautiful Days at the Movies.js
@@ -26,4 +26,22 @@ function processData(input) {
         reverseData = [];            
     }    
     console.log(cnt);    
+
+    /* Other algorithm */
+    /* https://www.hackerrank.com/michaellazell */
+    /*
+    var arr = input.split(' ');
+    var i = arr[0], j = arr[1], k = arr[2];
+    
+    var result = 0;
+
+    while(i <= j) {        
+        var rev = i.toString().split('').reverse().join('').toString();
+        if(Math.abs(parseInt(i) - parseInt(rev)) % k == 0) {
+            result++;
+        }
+        i++;
+    }
+    console.log(result);
+    */
 } 
